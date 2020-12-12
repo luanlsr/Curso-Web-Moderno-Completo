b940702f98970cadd211b9d7b376fc4e48a7a154
Luan da Silva Ramalho <luanfswd@gmail.com>
2020-12-12 05:31:53 -0300

    Inserido diretórios
    Aulas assistidas até tipos de váriáveis

---
+++ Meu Repositório/Exercícios JS/Variáveis e constantes.js
@@ -0,0 +1,21 @@
+var identificador = 'atributo' // o = atribui o valor à variável
+
+let identificador2 = 'atributo2'
+
+const identificador3 = 'atributo3'
+
+console.log(identificador);
+console.log(identificador2);
+console.log(identificador3);
+
+
+var a = 1
+let b = 2
+const c = 3
+
+var a = 4 //permite redeclarar e alterar
+b = 5 //não permite redeclarar mas permite alterar
+c //não permite redeclarar e alterar
+
+
+console.log(a, b, c);
