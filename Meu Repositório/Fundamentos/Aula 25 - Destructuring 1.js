10ecc3f9461b22f2423e7aea1020c1cafa651d8f
Luan da Silva Ramalho <luanfswd@gmail.com>
2021-01-08 13:46:26 -0300

    Destructuring

---
+++ Meu Repositório/Fundamentos/Aula 25 - Destructuring 1.js
@@ -0,0 +1,25 @@
+//novo recurso do ES6
+
+const pessoa = {
+    nome: 'Luan',
+    idade: 29,
+    endereco:{
+        logradouro: 'Rua São Gonçalo',
+        numero: 57
+    }
+}
+//tira as variáveis de dentro do objeto e usa como novas variáveis declaradas
+
+const {nome, idade} = pessoa
+console.log(nome, idade);
+
+//cria variáveis com o nome 'n' e 'i' e atribui os valores de nome e idade
+const {nome: n, idade: i} = pessoa
+console.log(n, i);
+
+const {sobrenome, bemHumorada = true} = pessoa
+
+console.log(sobrenome, bemHumorada);
+//undefined
+
+const {endereço: {logradouro, numero, cep }} = pessoa
